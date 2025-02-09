2b85620895f32251fb21f913514a3925ea4f5d5b
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-02-09 22:41:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+
+  let highest = -Infinity, secondHighest = -Infinity;
+
+  for (let num of arr) {
+    if (num > highest) {
+      secondHighest = highest;
+      highest = num;
+    } else if (num > secondHighest && num < highest) {
+      secondHighest = num;
+    }
+  }
